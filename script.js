f54d3b3626841c2f16f46173ce910ed8da7a1450
Ilya <101430971+poteryakin@users.noreply.github.com>
2024-11-17 21:48:26 +0300

    Add files via upload
    second update

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const screen = document.getElementById("screen");
 let btn = document.querySelectorAll(".button_number")
+let btn0 = document.querySelectorAll(".button-0")
 btn.forEach(b => b.addEventListener('click', getValue));
+btn0.forEach(b => b.addEventListener('click', getValue));
 
 function getValue(e) {
-  screen.innerText = screen.innerText + this.value;
+    // (screen.innerText.length < 9) ? screen.innerText = screen.innerText + this.value : screen.innerText = screen.innerText;
+    screen.innerText = screen.innerText + this.value;
 }
 
 function ac() {
@@ -12,8 +15,21 @@ function ac() {
 
 function eq() {
     if (screen.innerText != '') {
-        if (screen.innerText.indexOf('+') != -1) {
-            screen.innerText = screen.innerText.split('+').reduce((a,b) => +a + +b)
-        }
+        // if (screen.innerText.indexOf('+') != -1) {
+        //     screen.innerText = screen.innerText.split('+').reduce((a,b) => +a + +b)
+        // }
+        (screen.innerText.indexOf('+') != -1) ? screen.innerText = screen.innerText.split('+').reduce((a,b) => +a + +b) :
+            (screen.innerText.indexOf('*') != -1) ? screen.innerText = screen.innerText.split('*').reduce((a,b) => +a * +b) :
+                (screen.innerText.indexOf('-') != -1) ? screen.innerText = screen.innerText.split('-').reduce((a,b) => +a - +b) :
+                    (screen.innerText.indexOf('/') != -1) ? screen.innerText = screen.innerText.split('/').reduce((a,b) => (+a / +b).toFixed(5)) :
+                    screen.innerText = '';
     }
+}
+
+function root() {
+    (screen.innerText != '') ? screen.innerText = Math.sqrt(+screen.innerText) : screen.innerText = '';
+}
+
+function percent() {
+    (screen.innerText != '') ? screen.innerText = +screen.innerText / 100 : screen.innerText = '';
 }
